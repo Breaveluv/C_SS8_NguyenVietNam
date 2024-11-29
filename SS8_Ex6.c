3f78b5870227f54f309828dbc6029c6017ea64c4
Breaveluv <nam.bule12@gmail.com>
2024-11-29 16:17:41 +0700

    Update SS8_Ex6.c

--- SS8_Ex6.c
+++ SS8_Ex6.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main() {
     int a[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
     int sum = 0;
     for(int i = 0;i<4;i++){
